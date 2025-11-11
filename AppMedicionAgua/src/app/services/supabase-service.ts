4d5550625a8f26bd03287d38cbfbd9dc10ff85e2
Jose Pila <joselejandro_pilavizuete@hotmail.com>
2025-11-11 17:09:28 -0500

    UPDATE PROJECT

---
+++ AppMedicionAgua/src/app/services/supabase-service.ts
@@ -0,0 +1,78 @@
+import { Injectable, inject } from '@angular/core';
+import { createClient, type SupabaseClient, type Session } from '@supabase/supabase-js';
+import { BehaviorSubject } from 'rxjs';
+import { environment } from '../../environments/environment';
+@Injectable({
+  providedIn: 'root',
+})
+export class SupabaseService {
+  private supabase: SupabaseClient;
+  private _session$ = new BehaviorSubject<Session | null>(null);
+  session$ = this._session$.asObservable();
+
+  constructor() {
+    this.supabase = createClient(environment.supabaseURL, environment.supabaseAnonKey);
+
+    // Recupera sesión al iniciar
+    this.supabase.auth.getSession().then(({ data }) => this._session$.next(data.session));
+
+    // Escucha cambios de sesión (login/logout/refresh)
+    this.supabase.auth.onAuthStateChange((_event, session) => {
+      this._session$.next(session);
+    });
+  } 
+
+  get client() {
+    return this.supabase;
+  }
+
+  async signIn(email: string, password: string) {
+    const { data, error } = await this.supabase.auth.signInWithPassword({ email, password });
+    if (error) throw error;
+    return data;
+  }
+
+  async signUp(email: string, password: string) {
+    const { data, error } = await this.supabase.auth.signUp({ email, password });
+    if (error) throw error;
+    return data;
+  }
+
+  async signOut() {
+    const { error } = await this.supabase.auth.signOut();
+    if (error) throw error;
+  }
+
+  async getCurrentSession() {
+    const { data } = await this.supabase.auth.getSession();
+    return data.session;
+  }
+  /*
+  async getMessages() {
+  const { data, error } = await this.supabase
+    .from('messages')
+    .select('*')
+    .order('created_at', { ascending: true });
+  if (error) throw error;
+  return data;
+  }
+  async addMessage(content: string) {
+    const sesion = await this.getCurrentSession();
+    const email = sesion?.user.email;
+    const { error } = await this.supabase
+      .from('messages')
+      .insert({ content, autor:email });
+    if (error) throw error;
+  }
+  listenToMessages(callback: (payload: any) => void) {
+    return this.supabase
+     .channel('public:messages')
+     .on(
+       'postgres_changes',
+       { event: 'INSERT', schema: 'public', table: 'messages' },
+       payload => callback(payload.new)
+     )
+     .subscribe();
+  }
+*/
+}
