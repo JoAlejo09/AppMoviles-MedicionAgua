77258343c0f288814f3ad85c3d705f916d2aa1aa
Jose Pila <joselejandro_pilavizuete@hotmail.com>
2025-11-11 17:27:36 -0500

    UPDATE

--- AppMedicionAgua/src/app/home/home.page.ts
+++ AppMedicionAgua/src/app/home/home.page.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  IonContent, IonHeader, IonTitle, IonToolbar,
+  IonInput, IonItem, IonButton, IonLabel, IonList, IonText, IonSpinner
+} from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { SupabaseService } from 'src/app/services/supabase-service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [CommonModule, ReactiveFormsModule,
+    IonContent, IonHeader, IonTitle, IonToolbar,
+    IonInput, IonItem, IonButton, IonLabel, IonList, IonText, IonSpinner],
 })
 export class HomePage {
-  constructor() {}
+  //Inyeccion de dependencias en lugar del constructor
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private supa = inject(SupabaseService);
+  //Control de usuarios
+  loading = signal(false);
+  errorMsg = signal<string | null>(null);
+  infoMsg = signal<string | null>(null);
+  //Validaciones
+    form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+  async onLogin(){
+    this.resetMessages();
+    if (this.form.invalid) return;
+    this.loading.set(true);
+    const { email, password } = this.form.value;
+
+    try {
+      await this.supa.signIn(email!, password!);
+      await this.router.navigateByUrl('/home', { replaceUrl: true });
+    } catch (err: any) {
+      this.errorMsg.set(err.message ?? 'Error al iniciar sesión');
+    } finally {
+      this.loading.set(false);
+    }
+
+  }
+  private resetMessages() {
+    this.errorMsg.set(null);
+    this.infoMsg.set(null);
+  }
+
 }
--- AppMedicionAgua/src/environments/environment.ts
+++ AppMedicionAgua/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  supabaseURL:'',
-  supabaseAnonKey:''
+  supabaseURL:'https://bnybpwepufmgprlkkijf.supabase.co',
+  supabaseAnonKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJueWJwd2VwdWZtZ3BybGtraWpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4ODk5MTUsImV4cCI6MjA3ODQ2NTkxNX0.sEqOFiYny-neFUhxHIoaa1-yYX0OeIWjN7TcijcI6w0'
 };
 
 /*
