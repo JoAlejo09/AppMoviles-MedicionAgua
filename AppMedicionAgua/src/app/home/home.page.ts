400636dff6c186b0da7841c85f5e2a4751821053
josea <josealejandro_pilavizuete@hotmail.com>
2025-11-15 16:27:04 -0500

    UPDATE

--- AppMedicionAgua/src/app/home/home.page.ts
+++ AppMedicionAgua/src/app/home/home.page.ts
@@ -17,38 +17,6 @@ import { SupabaseService } from 'src/app/services/supabase-service';
     IonInput, IonItem, IonButton, IonLabel, IonList, IonText, IonSpinner],
 })
 export class HomePage {
-  //Inyeccion de dependencias en lugar del constructor
-  private fb = inject(FormBuilder);
-  private router = inject(Router);
-  private supa = inject(SupabaseService);
-  //Control de usuarios
-  loading = signal(false);
-  errorMsg = signal<string | null>(null);
-  infoMsg = signal<string | null>(null);
-  //Validaciones
-    form = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]]
-  });
-  async onLogin(){
-    this.resetMessages();
-    if (this.form.invalid) return;
-    this.loading.set(true);
-    const { email, password } = this.form.value;
 
-    try {
-      await this.supa.signIn(email!, password!);
-      await this.router.navigateByUrl('/home', { replaceUrl: true });
-    } catch (err: any) {
-      this.errorMsg.set(err.message ?? 'Error al iniciar sesión');
-    } finally {
-      this.loading.set(false);
-    }
-
-  }
-  private resetMessages() {
-    this.errorMsg.set(null);
-    this.infoMsg.set(null);
-  }
 
 }
