e7b2d60a1742ae9210a20243a92110c93f270920
Jose Pila <josealejandro_pilavizuete@hotmail.com>
2025-11-15 19:59:35 -0500

    UPDATE

--- AppMedicionAgua/src/app/home/home.page.ts
+++ AppMedicionAgua/src/app/home/home.page.ts
@@ -17,6 +17,38 @@ import { SupabaseService } from 'src/app/services/supabase-service';
     IonInput, IonItem, IonButton, IonLabel, IonList, IonText, IonSpinner],
 })
 export class HomePage {
+  //Creacion de formulario reactivo para login con validaciones
+  private fb  = inject(FormBuilder);
+  private router = inject(Router);
+  private supa = inject(SupabaseService);
+  //Estado de carga
+  loading = signal(false);
+  //Mensajes de error e informacion
+  errorMsg = signal<string | null>(null);
+  infoMsg = signal<string | null>(null);
 
-
+  //Definicion del formulario con validaciones
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+  //Metodo para manejar el login
+  async onLogin() {
+    this.resetMessages();
+    if (this.form.invalid) return;
+    this.loading.set(true);
+    const { email, password } = this.form.value;
+    try {
+      await this.supa.signIn(email!, password!);
+      await this.router.navigateByUrl('/home', { replaceUrl: true });
+    } catch (err: any) {
+      this.errorMsg.set(err.message ?? 'Error al iniciar sesión');
+    } finally {
+      this.loading.set(false);
+    }
+  }
+  private resetMessages() {
+    this.errorMsg.set(null);
+    this.infoMsg.set(null);
+  }
 }
--- AppMedicionAgua/src/app/services/supabase-service.ts
+++ AppMedicionAgua/src/app/services/supabase-service.ts
@@ -6,4 +6,41 @@ import { environment } from '../../environments/environment';
   providedIn: 'root',
 })
 export class SupabaseService {
+  // Definición de las propiedades y el constructor aquí
+  private supabase: SupabaseClient; // cliente
+  private _session$ = new BehaviorSubject<Session | null>(null); //control de ssesion
+  session$ = this._session$.asObservable();
+  //  
+  
+  constructor() {
+    this.supabase = createClient(environment.supabaseURL, environment.supabaseAnonKey);
+    // Recupera sesión al iniciar
+    this.supabase.auth.getSession().then(({ data }) => this._session$.next(data.session));
+    // Escucha cambios de sesión (login/logout/refresh)
+    this.supabase.auth.onAuthStateChange((_event, session) => {
+      this._session$.next(session);
+    }); 
+  }
+  //obtener cliente
+  get client() {
+    return this.supabase; 
+  }
+  // metodo para conectar usuario  
+  async signIn(email: string, password: string) {
+    const { data, error } = await this.supabase.auth.signInWithPassword({ email, password });
+    if (error) throw error;
+    return data;
+  }
+  // metodo para cerrar sesion
+  async signOut() {
+    const { error } = await this.supabase.auth.signOut();
+    if (error) throw error;
+  }
+  //oBTENER LA SESION ACTUAL
+  async getCurrentSession() {
+    const { data } = await this.supabase.auth.getSession();
+    return data.session;
+  }
+  
+  
 }
