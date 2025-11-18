ffe3dcbad9a543c6cf02173e9c188537e3b43de2
Jose Pila <josealejandro_pilavizuete@hotmail.com>
2025-11-17 23:22:53 -0500

    UPDATE ENVIROMENT

--- AppMedicionAgua/src/environments/environment.prod.ts
+++ AppMedicionAgua/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseURL:'https://bnybpwepufmgprlkkijf.supabase.co',
+  supabaseAnonKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJueWJwd2VwdWZtZ3BybGtraWpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4ODk5MTUsImV4cCI6MjA3ODQ2NTkxNX0.sEqOFiYny-neFUhxHIoaa1-yYX0OeIWjN7TcijcI6w0'
 };
